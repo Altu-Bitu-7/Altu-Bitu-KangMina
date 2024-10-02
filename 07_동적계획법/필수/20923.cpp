6f53feffe32ef2ba93a00e1dfe7e7c6518aa8aca
minae-mina <124029562+minae-mina@users.noreply.github.com>
2024-10-03 00:53:46 +0900

    [동적계획법] 1003 - 추가제출

--- 07_동적계획법/필수/20923.cpp
+++ 07_동적계획법/필수/20923.cpp
@@ -1,106 +1,92 @@
 // 숫자 할리갈리 게임
-// 추가제출 예정입니다.
+// 한 라운드의 조건을 잘못 생각했다 -> 문제를 꼼꼼히 읽기
+// typedef 활용
+// 카드더미의 위/아래 <-> 덱의 front/back 
 #include <iostream>
+#include <vector>
 #include <deque>
-#include <stack>
 using namespace std;
 
-// 승자를 찾아서 반환한다.
-string findWinner(const deque<int> &dodo, const deque<int> &su){
-    int d_size = dodo.size();
-    int s_size = su.size();
+typedef vector<deque<int>> cards;   // cards[0]: 도도, cards[1]: 수연
+const int DO = 0, SU = 1;
 
-    // 각자의 덱에 있는 카드의 개수가 같다면 비긴 것
-    if (d_size == s_size){
-        return "dosu";
-    }
-
-    // 자신의 덱에 더 많은 카드를 지닌 사람이 승자
-    string winner = (d_size > s_size) ? "do" : "su";
-    return winner;
-}
-
-/* 종치는 경우
-수연: 그라운드에 나와 있는 각각의 카드 더미에서 가장 위에 위치한 카드의 숫자 합이 5가 되는 순간
-(단, 어느 쪽의 그라운드도 비어있으면 안된다.)
-도도: 그라운드에 나와 있는 각각의 카드 더미 중 가장 위에 위치한 카드의 숫자가 5가 나오는 순간
-
-그라운드에 카드 내고나서 종치기 전에 덱 empty 여부 확인? (예제2)
-*/
-
-// 그라운드의 상황 파악
-// 0:아무일도 없음, 1: 도도가 종침,  2: 수연이가 종침
-int checkGround(stack<int> &d_ground, stack<int> &s_ground, deque<int> &dodo, deque<int> &su){
-    int d = d_ground.top();
-    int s = s_ground.top();
-
-    if (d == 5 || s == 5){   //도도가 종침
-        while (!s_ground.empty()) {
-            dodo.push_front(s_ground.top());
-            s_ground.pop();
-        }
-        while (!d_ground.empty()) {
-            dodo.push_front(d_ground.top());
-            d_ground.pop();
-        }
-        return 1;
-    }
-    else if(d+s == 5){  // 수연이가 종침
-        // 상대방의 그라운드에 있는 카드 더미를 뒤집어 자신의 덱 아래로 그대로 합친 후
-        while (!d_ground.empty()) {
-            su.push_front(d_ground.top());
-            d_ground.pop();
-        }
-        // 자신의 그라운드에 있는 카드 더미 역시 뒤집어 자신의 덱 아래로 그대로 가져와 합친다.
-        while (!s_ground.empty()) {
-            su.push_front(s_ground.top());
-            s_ground.pop();
-        }
-        return 2;
-    }
-    return 0;
-}
-
-// 할리갈리 게임을 m 라운드 진행하고 승자를 반환한다.
-string halliGalli(int n, int m, deque<int> &dodo, deque<int> &su){
-    int round = 0;  // 라운드 1개 끝날 때마다 +1
-    stack<int> d_ground;
-    stack<int> s_ground;
-
-    while ((round < m) && (!dodo.empty()) && (!su.empty())){
-        // 도도부터 카드 꺼낸다
-        int d_card = dodo.front();
-        dodo.pop_front();
-        d_ground.push(d_card);
-        
-        if (!checkGround(d_ground, s_ground, dodo, su)){
-            round++;
-        }
-
-        // 수연 카드 꺼낸다
-        int s_card = su.front();
-        su.pop_front();
-        s_ground.push(s_card);
-        
-        if (!checkGround(d_ground, s_ground, dodo, su)){
-            round++;
-        }
-        
-    }
-
-    // 게임 진행 도중 자신의 덱에 있는 카드의 수가 0개가 되면 상대방이 승리한 것
-    if (dodo.empty()){ // su.empty
-            return "su";    //do
-    } else {
+// 승리 판단하기
+string judge(cards& deck) {
+    int do_deck = deck[DO].size();
+    int su_deck = deck[SU].size();
+    // (도도가 덱에 가진 카드) > (수연이 덱에 가진 카드) 인 경우, (수연이의 덱이 빈 경우도 포함)
+    if (do_deck > su_deck) {
+        // 승자는 도도
         return "do";
     }
-
-    return findWinner(dodo, su);
+    // (도도가 덱에 가진 카드) < (수연이 덱에 가진 카드) 인 경우, (도도의 덱이 빈 경우도 포함)
+    else if (do_deck < su_deck) {
+        // 승자는 수연
+        return "su";
+    }
+    // (도도가 덱에 가진 카드) = (수연이 덱에 가진 카드) 인 경우
+    return "dosu";
 }
 
- // 덱의 front가 위, back이 아래라 하자.
- // 카드를 꺼낼 때는 덱의 front에서 꺼내서 뒤집는다.
- // 카드를 추가할 때는 덱의 back에다 한다.
+// 그라운드에서 덱으로 카드 옮기기
+void groundToDeck(deque<int> &deck, deque<int> &ground) {
+    // 그라운드의 카드를 뒤집에서 (먼저 내서 아래에 위치한 카드부터) 내 덱의 아래에 넣는다
+    // 즉,  그라운드의 back 에서 카드를 빼서 덱의 back 에 넣는다.
+    while (!ground.empty()) {
+        deck.push_back(ground.back());
+        ground.pop_back();
+    }
+}
+
+// 종을 쳤을 때: 카드를 그라운드에서 덱으로 가져간다
+void ringTheBell(int player, cards& deck, cards& ground) {
+    // 상대방 그라운드의 카드를 먼저 내 덱에 합치고
+    groundToDeck(deck[player], ground[!player]);
+    // 내 그라운드의 카드를 내 덱에 합친다
+    groundToDeck(deck[player], ground[player]);
+}
+
+// 종을 울릴 수 있는 사람 판단
+int whoCanRingTheBell(cards& deck, cards& ground) {
+    // 벡터의 값 접근할 때 empty 체크 잊지말고 하자!
+    // 값이 5인 카드가 나왔다 -> 도도가 종을 친다 (0)
+    if (!ground[DO].empty() && ground[DO].front() == 5){    // 도도가 낸 카드가 5
+        return DO;
+    }
+    if (!ground[SU].empty() && ground[SU].front() == 5){    // 수연이 낸 카드가 5
+        return DO;
+    }
+    // 그라운드의 두 값을 합하면 5이다 -> 수연이 종을 친다 (1)
+    if (!ground[DO].empty() && !ground[SU].empty() && (ground[DO].front() + ground[SU].front() == 5)){    
+        return SU;
+    }
+    // 위의 어떤 경우도 해당하지 않으면, 아무도 종을 치지 못한다. (-1)
+    return -1;
+}
+
+// 할리갈리 게임 진행
+string halliGalli(int m, cards& deck, cards& ground) {
+    bool turn = DO; // 도도가 먼저 게임을 시작한다
+    while (m--) {// 게임을 m 라운드 진행한다
+        // 카드 내려놓기 (덱 -> 그라운드)
+        ground[turn].push_front(deck[turn].front());
+        deck[turn].pop_front();
+        // 덱이 비는지 체크
+        if (deck[turn].empty()){
+            break;
+        }
+        // 종을 울릴 수 있는 사람 체크
+        int bell = whoCanRingTheBell(deck, ground);
+        // 종을 울린 경우 카드 가져가기 (그라운드 -> 덱)
+        if (bell != -1){
+            ringTheBell(bell, deck, ground);
+        }
+        // 차례 바꾸기
+        turn = !turn;   
+    }
+    // m 라운드가 끝나거나, 누군가의 덱이 비었을 때,
+    return judge(deck); // 승자를 판단해 반환한다.
+}
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -108,8 +94,7 @@ int main(){
 
     int n;  // n: 도도와 수연이가 가지는 카드의 개수
     int m;  // m: 게임 진행 횟수
-    deque<int> dodo;    // dodo: 도도의 카드 더미
-    deque<int> su;  // su: 수연의 카드 더미
+    cards deck(2), ground(2);   // [0]: 도도, [1]: 수연
     string answ;    // answ: 도도가 이기면 "do", 수연이 이기면 "su", 비기면 "dosu"
 
     // 입력
@@ -117,13 +102,14 @@ int main(){
     int d, s;
     for (int i = 0; i < n; i++){
         // 덱의 맨 아래에 위치한 카드에 적혀 있는 수부터 입력받음
+        // 덱의 front가 위, back이 아래라 하자.
         cin >> d >> s;
-        dodo.push_back(d);
-        su.push_back(s);
+        deck[DO].push_front(d);
+        deck[SU].push_front(s);
     }
 
     // 연산
-    answ = halliGalli(n, m, dodo, su);
+    answ = halliGalli(m, deck, ground);
 
     // 출력
     cout << answ << '\n';   
